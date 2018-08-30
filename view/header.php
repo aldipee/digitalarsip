2ba29fdb0891d63159d540c6757903d3ca8b91af
aldipee <aldipeee@gmail.com>
2018-08-30 14:24:13 +0700

    Minify CSS

--- view/header.php
+++ view/header.php
@@ -13,46 +13,42 @@
 
         <!-- PACE JS -->
         <script data-pace-options='{ "ajax": false }' src="<?= BASE_URL;?>view/plugins/pace/pace.js"></script>
-	    <link href="<?= BASE_URL;?>view/plugins/pace/themes/orange/pace-theme-minimal.css" rel="stylesheet">  
+	    <link href="<?= BASE_URL;?>view/plugins/pace/themes/orange/pace-theme.minimal.min.css" rel="stylesheet">  
 
         <!-- OFFLINE JS -->
         <script src="<?= BASE_URL;?>view/plugins/offline/offline.min.js"></script>
-	    <link href="<?= BASE_URL;?>view/plugins/offline/english.css" rel="stylesheet"> 
-        <link href="<?= BASE_URL;?>view/plugins/offline/offline-theme-slide.css" rel="stylesheet">  
+	    <link href="<?= BASE_URL;?>view/plugins/offline/engslish.min.css" rel="stylesheet"> 
+        <link href="<?= BASE_URL;?>view/plugins/offline/offline-theme-slide.min.css" rel="stylesheet">  
 
 
         <!-- Google Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&subset=latin,cyrillic-ext" rel="stylesheet" type="text/css">
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
         <!-- Bootstrap Core Css -->
-        <link href="<?= BASE_URL;?>view/plugins/bootstrap/css/bootstrap.css" rel="stylesheet">
+        <link href="<?= BASE_URL;?>view/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
         <!-- Waves Effect Css -->
-        <link href="<?= BASE_URL;?>view/plugins/node-waves/waves.css" rel="stylesheet" />
+        <link href="<?= BASE_URL;?>view/plugins/node-waves/waves.min.css" rel="stylesheet" />
  
         <!-- Animation Css -->
-        <link href="<?= BASE_URL;?>view/plugins/animate-css/animate.css" rel="stylesheet" />
+        <link href="<?= BASE_URL;?>view/plugins/animate-css/animate.min.css" rel="stylesheet" />
 
         <!-- Sweet Alert Css -->
-        <link href="<?= BASE_URL;?>view/plugins/sweetalert/sweetalert.css" rel="stylesheet" />
+        <link href="<?= BASE_URL;?>view/plugins/sweetalert/sweetalert.min.css" rel="stylesheet" />
 
         <!-- Custom Css -->
-        <link href="<?= BASE_URL;?>view/css/style.css" rel="stylesheet">
+        <link href="<?= BASE_URL;?>view/css/style.minimal.css" rel="stylesheet">
 
         <!-- JQuery DataTable Css -->
-        <link href="<?= BASE_URL;?>view/plugins/jquery-datatable/skin/bootstrap/css/dataTables.bootstrap.css" rel="stylesheet">
+        <link href="<?= BASE_URL;?>view/plugins/jquery-datatable/skin/bootstrap/css/dataTables.bootstrap.min.css" rel="stylesheet">
 
         <!-- Light Gallery Plugin Css -->
-        <link href="<?= BASE_URL;?>view/plugins/light-gallery/css/lightgallery.css" rel="stylesheet">
+        <link href="<?= BASE_URL;?>view/plugins/light-gallery/css/lightgallery.min.css" rel="stylesheet">
 
-        <link href="<?= BASE_URL;?>view/plugins/bootstrap-select/css/bootstrap-select.css" rel="stylesheet" />
+        <link href="<?= BASE_URL;?>view/plugins/bootstrap-select/css/bootstrap-select.min.css" rel="stylesheet" />
         <!-- AdminBSB Themes. You can choose a theme from css/themes instead of get all themes -->
-        <link href="<?= BASE_URL;?>view/css/themes/all-themes.css" rel="stylesheet" />
-
-        <!-- Dropzone Css -->
-        <link href="<?= BASE_URL;?>view/plugins/dropzone/dropzone.css" rel="stylesheet">
-
+        <link href="<?= BASE_URL;?>view/css/themes/all-themes.min.css" rel="stylesheet" />
         <!-- Multi Select Css -->
-        <link href="<?= BASE_URL;?>view/plugins/multi-select/css/multi-select.css" rel="stylesheet">
+        <link href="<?= BASE_URL;?>view/plugins/multi-select/css/multi-select.min.css" rel="stylesheet">
 
 
     </head>
